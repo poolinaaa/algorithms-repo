8450ab4243361a701ced6c89a0cc69c8c4173951
Pola Borkowska <poolaborkowska@gmail.com>
2024-07-17 21:17:32 +0200

    m

--- heaps.py
+++ heaps.py
@@ -3,10 +3,10 @@ def heapify(arr, idx):
 
     L = 2*idx + 1
     R = 2*idx + 2
-    
+
     if idx >= len(arr)//2:
         return arr
-        
+
     if arr[curr] < arr[L]:
         curr = L
     if arr[curr] < arr[R]:
@@ -15,16 +15,27 @@ def heapify(arr, idx):
         parent_to_be = arr[curr]
         arr[curr] = arr[idx]
         arr[idx] = parent_to_be
-        heapify(arr, curr)        
+        heapify(arr, curr)
     return arr
+
 
 def build_heap(arr):
     i = 0
     while (len(arr)//2)-i >= 0:
         arr = heapify(arr, (len(arr)//2)-i)
-        i+=1
+        i += 1
     return arr
 
-a = [2,5,4,3,1,8,9,3,1,9,8]
+def del_node(arr, idx):
+    arr[idx]=arr.pop()
+    return heapify(arr,idx)
+
+
+
+a = [2, 5, 4, 3, 1, 8, 9, 3, 1, 9, 8]
 heap = build_heap(a)
 print(heap)
+delated  = del_node(heap, 7)
+print(delated)
+delated  = del_node(heap, 4)
+print(delated)
